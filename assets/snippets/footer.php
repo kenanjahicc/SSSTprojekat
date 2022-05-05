a337ca91274b2109d05b6c797e32efa0055d4a25
exam <exam@students.local>
2022-05-05 14:08:35 +0200

    next

---
+++ assets/snippets/footer.php
@@ -0,0 +1,7 @@
+//generate footer div using bootstrap
+<div class="container">
+    <hr>
+    <footer>
+        <p>&copy; Company 2014</p>
+    </footer>
+</div>
--- index.php
+++ index.php
@@ -6,13 +6,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style/style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+<!--  load bootstrap-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
     <div class="fullpagewrap">
-        //php includes header.php
-        <?php include 'assets/snippets/header.php'; ?>
         
+        <?php include 'assets/snippets/header.php'; ?>
+        <main>
+        <div class="item">
+            <img src="https://th.bing.com/th/id/R.7806d458d66dfcb9d5b2e0faf2c57e8c?rik=PQzXIoa534LiWQ&pid=ImgRaw&r=0" alt="" class="slicica">
+            <h3 class="naslov">lorem ipsum</h3>
+            <p class="opis">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+        </div>
+        <div class="item">
+            <img src="https://th.bing.com/th/id/R.7806d458d66dfcb9d5b2e0faf2c57e8c?rik=PQzXIoa534LiWQ&pid=ImgRaw&r=0" alt="" class="slicica">
+            <h3 class="naslov">lorem ipsum</h3>
+            <p class="opis">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+        </div>
+        <div class="item">
+            <img src="https://th.bing.com/th/id/R.7806d458d66dfcb9d5b2e0faf2c57e8c?rik=PQzXIoa534LiWQ&pid=ImgRaw&r=0" alt="" class="slicica">
+            <h3 class="naslov">lorem ipsum</h3>
+            <p class="opis">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+        </div>
+        <div class="item">
+            <img src="https://th.bing.com/th/id/R.7806d458d66dfcb9d5b2e0faf2c57e8c?rik=PQzXIoa534LiWQ&pid=ImgRaw&r=0" alt="" class="slicica">
+            <h3 class="naslov">lorem ipsum</h3>
+            <p class="opis">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+        </div>
+        <div class="item">
+            <img src="https://th.bing.com/th/id/R.7806d458d66dfcb9d5b2e0faf2c57e8c?rik=PQzXIoa534LiWQ&pid=ImgRaw&r=0" alt="" class="slicica">
+            <h3 class="naslov">lorem ipsum</h3>
+            <p class="opis">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+        </div>
+        <div class="item">
+            <img src="https://th.bing.com/th/id/R.7806d458d66dfcb9d5b2e0faf2c57e8c?rik=PQzXIoa534LiWQ&pid=ImgRaw&r=0" alt="" class="slicica">
+            <h3 class="naslov">lorem ipsum</h3>
+            <p class="opis">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+        </div>
+        </main>
+        <?php include 'assets/snippets/footer.php'; ?>
+
     </div>
     
 
